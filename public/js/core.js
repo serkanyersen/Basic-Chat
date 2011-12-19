60cc141ee01038aa8e5cf503cf37e035850829d8
Serkan Yerşen <serkanyersen@gmail.com>
2011-12-19 17:27:18 +0000

    fix websocket port

--- public/js/core.js
+++ public/js/core.js
@@ -110,7 +110,7 @@ var Chat = {
     },
     
     init: function(){
-        this.socket = io.connect(location.href);
+        this.socket = io.connect(location.href); 
         this.setSocketEvents();
         this.setName();
         this.joined();
